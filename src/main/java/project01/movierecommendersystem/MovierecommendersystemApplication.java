34d81da605c4a7fcfcd4d6a9bb7b67719c0d4c37
Srk Prasad Changala <srkprasadchangala@Srks-Mac-mini.local>
2025-03-16 19:40:04 -0400

    first commit

---
+++ src/main/java/project01/movierecommendersystem/MovierecommendersystemApplication.java
@@ -0,0 +1,13 @@
+package project01.movierecommendersystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovierecommendersystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovierecommendersystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project01/movierecommendersystem/MovierecommendersystemApplicationTests.java
@@ -0,0 +1,13 @@
+package project01.movierecommendersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovierecommendersystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
