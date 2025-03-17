696ebd944f8dfa6e3b5d2437b2ae36b7300f4e71
Srk Prasad Changala <srkprasadchangala@Srks-Mac-mini.local>
2025-03-16 21:13:21 -0400

    seen movie constructor only when needed after declaring prototype scope with proxy injection

--- src/main/java/project01/movierecommendersystem/Movie.java
+++ src/main/java/project01/movierecommendersystem/Movie.java
@@ -2,10 +2,11 @@ package project01.movierecommendersystem;
 
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+@Scope(value=ConfigurableBeanFactory.SCOPE_PROTOTYPE, proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class Movie {
     //for keeping track of instances created
     private static int instances = 0;
@@ -22,5 +23,12 @@ public class Movie {
 
     public static int getInstances() {
         return Movie.instances;
+    }
+
+    public double movieSimilarity(int movie1, int movie2) {
+
+        double similarity = 0.0;
+
+        return similarity;
     }
 }
